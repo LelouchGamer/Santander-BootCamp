19b0bd67fb0f1b7af15026f6fa7261fad9f83470
LelouchGamer <igor.grilos@gmail.com>
2025-12-04 06:55:08 -0300

    Commit 7

---
+++ Tipos de Operadores Com Python/Comparacao.py
@@ -0,0 +1,8 @@
+saldo = 20
+saque = 20
+print(saldo == saque)  # Igualdade
+print(saldo != saque)  # Diferença
+print(saldo > saque)   # Maior
+print(saldo < saque)   # Menor
+print(saldo >= saque)  # Maior ou igual
+print(saldo <= saque)  # Menor ou igual
--- Tipos de Operadores Com Python/Logicos.py
+++ Tipos de Operadores Com Python/Logicos.py
@@ -2,3 +2,7 @@ saldo = 1000
 saque = 200
 limite = 100
 saldo >= saque and saque <= limite
+saldo >= saque or saque <= limite
+not saque > limite
+conta_especial = True
+saldo >= saque or (not conta_especial and saque <= limite)
